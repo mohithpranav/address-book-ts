adf90cf2ad02878f67afad75ab8ebf787e2c7feb
Mohith Pranav <pranavmohith@gmail.com>
2025-12-18 17:45:13 +0530

    UC1: Add Contact class with person details

--- project/index.ts
+++ project/index.ts
@@ -1,1 +1,37 @@
 console.log("Welcome to the Address Book Application!");
+
+// UC1: Create Contact class with person details
+class Contact {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public address: string,
+    public city: string,
+    public state: string,
+    public zipCode: string,
+    public phone: string,
+    public email: string
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  toString(): string {
+    return `
+Name: ${this.fullName()}
+Address: ${this.address}, ${this.city}, ${this.state}, ${this.zipCode}
+Phone: ${this.phone}
+Email: ${this.email}
+    `;
+  }
+}
